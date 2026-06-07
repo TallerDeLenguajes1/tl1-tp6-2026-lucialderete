83bb4f754c79da9ac4a80b47ce3e1e5e9ecee3e9
Lucia Alderete <lu.4ld3r3t3@gmail.com>
2026-06-07 02:35:35 -0300

    va de nuevo

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
-﻿/*Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
 b=a;
 Console.WriteLine("valor de a: "+a);
 Console.WriteLine("valor de b: "+b);
-*/
 
-Console.Write("Ingrese un numero positivo: ");
+
+Console.WriteLine("Ingrese un numero positivo: ");
 string numeroIngresado = Console.ReadLine();
-int numero;
-if(int.TryParse(numeroIngresado, out numero)){
+if(int.TryParse(numeroIngresado, out int numero)){
     if(numero > 0){
         int invertido=0;
+        int aux = numero;
         while(numero > 0){
-            int aux = numero;
             int resto = numero %10;
-            invertido =(invertido *10)+resto;
-            aux /= 10;
+            invertido =(invertido * 10)+resto;
+            numero /= 10;
         }
         Console.WriteLine($"Numero invertido: {invertido}");
-        Console.WriteLine($"Numero ingresado: {numero}");
+        Console.WriteLine($"Numero ingresado: {aux}");
 
     }else{
         Console.WriteLine("Ingrese un numero positivo");
